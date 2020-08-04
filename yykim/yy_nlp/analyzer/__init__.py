882c4a7d3af90482b667b24d6307010c64593079
nanimo22 <kyu6286@gmail.com>
2020-08-04 17:37:53 +0900

    패키지에 LSA class 코드 추가

--- yykim/yy_nlp/analyzer/__init__.py
+++ yykim/yy_nlp/analyzer/__init__.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 import copy
+
+from konlpy.tag import Kkma
+
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB 
 from sklearn.model_selection import GridSearchCV
-from konlpy.tag import Kkma
+from sklearn.decomposition import TruncatedSVD
+
+import seaborn as sns
