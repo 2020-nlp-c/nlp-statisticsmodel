6d01e9949d621dc5cafd204992379dde0e2700b3
BlueMary <bigboom1253@naver.com>
2020-08-06 11:49:34 +0900

    LSA,LDA 직접구현 추가

--- BaeJinwoo/NLC_pakage/preprocessor/tokenizer.py
+++ BaeJinwoo/NLC_pakage/preprocessor/tokenizer.py
@@ -1,7 +1,10 @@
 class Tokenizer :
-    def tokenizer(document):
+
+    def eng_tokenizer(self, document):
     #pip install nltk ## 오류문구 해주기
         
+        print("nltk 라이브러리를 사용하기 때문에 없을 경우 다운 받을 수 있습니다. 기다려주세요.") ##있어도 다운받아짐. 고치기
+
         import nltk
         nltk.download('punkt')
         from nltk.tokenize import word_tokenize
@@ -11,4 +14,20 @@ class Tokenizer :
         word_tokens = word_tokenize(text)
         return word_tokens
 
- 
+
+    ## 직접 konlpy를 가져다 쓰면되지만, 전부 외울 수 없기 때문에 편의성을 위해 개인 라이브러리에 저장하는 용도
+
+    # def kor_tokenizer(self, document):
+
+    #     print("konlypy 라이브러리를 사용하기 때문에 없을 경우 다운 받을 수 있습니다. 기다려주세요.") ##있어도 다운받아짐. 고치기
+
+    #     import konlpy
+    #     from konlpy.tag import Komoran
+    #     komoran= Komoran()
+    #     kor_text = document
+    #     komoran_tokens = komoran.morphs(kor_text)
+    #     return komoran_tokens
+
+    ###불용어(Stopword) 처리
+
+    ###N-gram
